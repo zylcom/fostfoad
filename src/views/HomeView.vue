6db7b91659745f70223a93274dcc284ba3447348
sabilillah <sabilillah272@gmail.com>
2022-08-17 00:02:05 +0700

    refactor(views): update home page
    Add cart button component.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,12 @@
 <script setup>
 import SearchBar from "../components/SearchBar.vue";
+import CartButton from "../components/CartButton.vue";
 </script>
 
 <template>
-  <header class="mx-5 pt-5">
+  <header class="mx-5 flex gap-x-2 pt-5">
     <SearchBar />
+    <CartButton />
   </header>
   <main class="mx-[18px]">
     <h1>Home Page</h1>
