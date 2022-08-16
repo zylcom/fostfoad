f07c1c2fbd6a646f76a030008881f2e338cc9275
sabilillah <sabilillah272@gmail.com>
2022-08-16 21:45:48 +0700

    feat(components): add search filter button

---
+++ src/components/SearchFilterButton.vue
@@ -0,0 +1,9 @@
+<script setup>
+import IconFilter from "./icons/IconFilter.vue";
+</script>
+
+<template>
+  <button class="absolute right-3 top-1/2 -mt-2.5">
+    <IconFilter class="w-5 h-5" />
+  </button>
+</template>
