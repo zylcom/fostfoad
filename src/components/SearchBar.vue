50a03a3bd4f8d34ab6536dc5aa90bf7d07679c39
sabilillah <sabilillah272@gmail.com>
2022-08-16 22:52:39 +0700

    refactor(components): update search bar component
    Add search filter button component to search bar component

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,9 +1,20 @@
-<script setup></script>
+<script setup>
+import IconSearch from "./icons/IconSearch.vue";
+import SearchFilterButton from "./SearchFilterButton.vue";
+</script>
 
 <template>
-  <input
-    type="text"
-    class="w-full h-10 bg-white rounded-[10px] pl-6 px-1 focus:ring-0 focus:outline-none"
-    placeholder="Search foods"
-  />
+  <div class="flex relative">
+    <input
+      type="text"
+      class="w-full h-10 bg-white rounded-[10px] pl-8 focus:ring-1 focus:outline-none placeholder:text-sm placeholder:italic"
+      placeholder="Search foods"
+    />
+
+    <span class="absolute left-3 top-1/2 -mt-2">
+      <IconSearch class="w-4 h-4" />
+    </span>
+
+    <SearchFilterButton />
+  </div>
 </template>
