5584957ab258838a88b86cfb6512087b97693236
sabilillah <sabilillah272@gmail.com>
2022-08-18 11:34:50 +0700

    feat(components): add navigation bar

---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,34 @@
+<script setup>
+import IconHome from "../components/icons/IconHome.vue";
+import IconMenu from "../components/icons/IconMenu.vue";
+import IconHistory from "../components/icons/IconHistory.vue";
+import IconDotsHorizontal from "../components/icons/IconDotsHorizontal.vue";
+</script>
+
+<template>
+  <nav class="flex justify-around">
+    <button class="flex flex-col items-center justify-end">
+      <IconHome />
+
+      <span class="text-[length:9px]">Home</span>
+    </button>
+
+    <button class="flex flex-col items-center justify-end">
+      <IconMenu />
+
+      <span class="text-[length:9px]">Menu</span>
+    </button>
+
+    <button class="flex flex-col items-center justify-end">
+      <IconHistory />
+
+      <span class="text-[length:9px]">History</span>
+    </button>
+
+    <button class="flex flex-col items-center justify-end">
+      <IconDotsHorizontal />
+
+      <span class="text-[length:9px]">More</span>
+    </button>
+  </nav>
+</template>
