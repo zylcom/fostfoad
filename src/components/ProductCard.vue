4a4c89259422b2df9e76cabd70f2d0cefcc636c5
sabilillah <sabilillah272@gmail.com>
2022-08-18 20:39:08 +0700

    feat(components): add product card
    create product card component

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,22 @@
+<script setup>
+defineProps({
+  image: String,
+  name: String,
+  price: Number,
+});
+</script>
+
+<template>
+  <figure>
+    <img
+      :src="image"
+      :alt="name"
+      class="w-full object-cover object-center shadow-md"
+    />
+
+    <figcaption>
+      <h4 class="text-xs">{{ name }}</h4>
+      <span class="text-xs">{{ price }}</span>
+    </figcaption>
+  </figure>
+</template>
