75ffec3cbf2ddd166d97d95f54fe1607466e50e6
sabilillah <sabilillah272@gmail.com>
2022-08-19 13:58:36 +0700

    feat(components): add menu list

---
+++ src/components/MenuList.vue
@@ -0,0 +1,32 @@
+<script setup>
+import MenuItem from "../components/MenuItem.vue";
+</script>
+
+<template>
+  <div class="menu-list mt-12 flex flex-col gap-y-3 pb-14">
+    <MenuItem product-name="Espresso Single1" :price="15000" />
+    <MenuItem product-name="Espresso Single2" :price="15000" />
+    <MenuItem product-name="Espresso Single3" :price="15000" />
+    <MenuItem product-name="Espresso Single4" :price="15000" />
+    <MenuItem product-name="Espresso Single5" :price="15000" />
+    <MenuItem product-name="Espresso Single6" :price="15000" />
+    <MenuItem product-name="Espresso Single1" :price="15000" />
+    <MenuItem product-name="Espresso Single2" :price="15000" />
+    <MenuItem product-name="Espresso Single3" :price="15000" />
+    <MenuItem product-name="Espresso Single4" :price="15000" />
+    <MenuItem product-name="Espresso Single5" :price="15000" />
+    <MenuItem product-name="Espresso Single6" :price="15000" />
+    <MenuItem product-name="Espresso Single1" :price="15000" />
+    <MenuItem product-name="Espresso Single2" :price="15000" />
+    <MenuItem product-name="Espresso Single3" :price="15000" />
+    <MenuItem product-name="Espresso Single4" :price="15000" />
+    <MenuItem product-name="Espresso Single5" :price="15000" />
+    <MenuItem product-name="Espresso Single6" :price="15000" />
+    <MenuItem product-name="Espresso Single1" :price="15000" />
+    <MenuItem product-name="Espresso Single2" :price="15000" />
+    <MenuItem product-name="Espresso Single3" :price="15000" />
+    <MenuItem product-name="Espresso Single4" :price="15000" />
+    <MenuItem product-name="Espresso Single5" :price="15000" />
+    <MenuItem product-name="Espresso Single6" :price="15000" />
+  </div>
+</template>
