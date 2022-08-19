1415e741ec3340f4eb5f6adcb2919506becfba11
sabilillah <sabilillah272@gmail.com>
2022-08-19 10:48:10 +0700

    feat(components): add pill button
    Create pill button component.

---
+++ src/components/PillButton.vue
@@ -0,0 +1,13 @@
+<script setup>
+defineProps({
+  text: String,
+});
+</script>
+
+<template>
+  <button
+    class="h-9 rounded-full bg-charolais-cattle px-12 text-center font-roboto text-sm text-gray-500"
+  >
+    {{ text }}
+  </button>
+</template>
