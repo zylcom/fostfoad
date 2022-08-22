3b2debc9e4999dfa26f0e13bfbd87fc0bcdf4cac
sabilillah <sabilillah272@gmail.com>
2022-08-22 15:46:21 +0700

    feat(components): add navigation button
    Create navigation button.

---
+++ src/components/NavigationButton.vue
@@ -0,0 +1,22 @@
+<script setup>
+defineProps({
+  path: String,
+  pathName: String,
+  icon: Object,
+  textActiveColor: String,
+});
+</script>
+
+<template>
+  <router-link :to="path" custom v-slot="{ isActive, navigate }">
+    <button class="flex flex-col items-center justify-end" @click="navigate">
+      <component :is="icon" :is-active="isActive" />
+
+      <span
+        :class="isActive ? textActiveColor : 'text-dark-tone-ink/90'"
+        class="text-[length:9px]"
+        >{{ pathName }}</span
+      >
+    </button>
+  </router-link>
+</template>
