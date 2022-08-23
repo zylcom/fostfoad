b353897ed4da54fc66ea4b9c1ad4b367a4ad993b
sabilillah <sabilillah272@gmail.com>
2022-08-23 11:39:38 +0700

    update src/components/NavigationBar.vue

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -3,32 +3,37 @@ import IconHome from "../components/icons/IconHome.vue";
 import IconMenu from "../components/icons/IconMenu.vue";
 import IconHistory from "../components/icons/IconHistory.vue";
 import IconDotsHorizontal from "../components/icons/IconDotsHorizontal.vue";
+import NavigationButton from "./NavigationButton.vue";
 </script>
 
 <template>
   <nav class="flex justify-around">
-    <button class="flex flex-col items-center justify-end">
-      <IconHome />
+    <NavigationButton
+      path="/"
+      path-name="Home"
+      :icon="IconHome"
+      text-active-color="text-chocobo-feather"
+    />
 
-      <span class="text-[length:9px]">Home</span>
-    </button>
+    <NavigationButton
+      path="/menu"
+      path-name="Menu"
+      :icon="IconMenu"
+      text-active-color="text-torii-red"
+    />
 
-    <button class="flex flex-col items-center justify-end">
-      <IconMenu />
+    <NavigationButton
+      path="/history"
+      path-name="History"
+      :icon="IconHistory"
+      text-active-color="text-heirloom-hydrangea"
+    />
 
-      <span class="text-[length:9px]">Menu</span>
-    </button>
-
-    <button class="flex flex-col items-center justify-end">
-      <IconHistory />
-
-      <span class="text-[length:9px]">History</span>
-    </button>
-
-    <button class="flex flex-col items-center justify-end">
-      <IconDotsHorizontal />
-
-      <span class="text-[length:9px]">More</span>
-    </button>
+    <NavigationButton
+      path="/setting"
+      path-name="More"
+      :icon="IconDotsHorizontal"
+      text-active-color="text-venetian-nights"
+    />
   </nav>
 </template>
