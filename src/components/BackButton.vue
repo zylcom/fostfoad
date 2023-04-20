f82e069d8ac965101c6d8c14f0754d7d2925105d
sabilillah <sabilillah272@gmail.com>
2023-04-20 14:48:49 +0700

    perf(components): update back button

--- src/components/BackButton.vue
+++ src/components/BackButton.vue
@@ -1,35 +1,16 @@
 <script setup>
-import { onMounted, ref } from "vue";
 import { useRouter } from "vue-router";
 import IconArrow from "./icons/IconArrow.vue";
 
-const routePath = ref("/menu");
 const router = useRouter();
 
-onMounted(() => {
-  if (
-    router.options.history.state.back === null ||
-    router.options.history.state.back === "/result" ||
-    /result/gi.test(router.currentRoute.value.fullPath)
-  ) {
-    routePath.value = "/";
-  } else if (
-    router.options.history.state.back === "/login" ||
-    router.options.history.state.back === "/register"
-  ) {
-    routePath.value = "/settings";
-  } else if (
-    router.options.history.state.back !== "/cart" &&
-    router.options.history.state.back !== "/login" &&
-    router.options.history.state.back !== "/register"
-  ) {
-    routePath.value = router.options.history.state.back;
-  }
-});
+function goBack() {
+  router.go(-1);
+}
 </script>
 
 <template>
-  <RouterLink :to="routePath" class="transition duration-300 hover:scale-125">
+  <button class="transition duration-300 hover:scale-125" @click="goBack">
     <IconArrow class="h-6 w-6" />
-  </RouterLink>
+  </button>
 </template>
