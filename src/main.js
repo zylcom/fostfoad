3fa91dd09aabc0a0f1d996ab0bc9c816c21bc363
sabilillah <sabilillah272@gmail.com>
2023-05-01 21:09:38 +0700

    chore(deps): update deps
    Update vue-tel-input from v6.0.5 to v8.1.1

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp, h, provide } from "vue";
 import { createPinia } from "pinia";
 import { DefaultApolloClient } from "@vue/apollo-composable";
 import VueTelInput from "vue-tel-input";
-import "vue-tel-input/dist/vue-tel-input.css";
+import "vue-tel-input/vue-tel-input.css";
 
 import apolloClient from "./plugins/apollo";
 import App from "./App.vue";
