2d43a755c26372ed9367585262cc3d937474e70f
sabilillah <sabilillah272@gmail.com>
2023-05-01 21:04:04 +0700

    fix vitest warn

--- test/components/RegisterInput.test.js
+++ test/components/RegisterInput.test.js
@@ -5,7 +5,11 @@ describe("RegisterInput.vue Test", () => {
   const name = "Test Register";
   const email = "test@register.com";
   const password = "secret";
-  const wrapper = mount(RegisterInput);
+  const wrapper = mount(RegisterInput, {
+    global: {
+      components: { "vue-tel-input": { template: "<div></div>" } },
+    },
+  });
 
   test("if name input filled, name ref will have same value", async () => {
     await wrapper.find("#name").setValue(name);
