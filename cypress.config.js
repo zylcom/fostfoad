ef8165a884fb9e21a54f613c978822316d790940
sabilillah <sabilillah272@gmail.com>
2023-05-02 12:44:21 +0700

    chore: update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   defaultCommandTimeout: 10000,
   responseTimeout: 10000,
+  requestTimeout: 10000,
   pageLoadTimeout: 100000,
   e2e: {
     specPattern: "cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}",
